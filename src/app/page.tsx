104060f5c93985aca2c6ba5347d2a07b4ccee568
gregor-J8 <gregor.johnson@outlook.com>
2025-07-29 16:08:22 -0500

    added loading spinner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,18 @@
+import { LoadingPage } from "@/components/ui/loading";
+import { useUser } from "@/hooks/useAuth";
+import { useRouter } from "next/router";
+
 export default function Home() {
+    const router = useRouter()
+    const token = localStorage.getItem("token")
+    const { data, isLoading, error } = useUser(token)
+
+    if (error) {
+        router.push("/login")
+    }
+    if (isLoading) {
+        <LoadingPage/>
+    }
     return (
         <div>
         
