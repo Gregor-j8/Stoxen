89d25472e3f23fda610cd5f90facb7f869d255fc
gregor-J8 <gregor.johnson@outlook.com>
2025-07-29 16:30:32 -0500

    added token fetch for main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,23 @@
-import { LoadingPage } from "@/components/ui/loading";
-import { useUser } from "@/hooks/useAuth";
-import { useRouter } from "next/router";
+"use client"
+import { LoadingPage } from "@/components/ui/loading"
+import { useUser } from "@/hooks/useAuth"
+import { useRouter } from "next/navigation"
 
 export default function Home() {
     const router = useRouter()
     const token = localStorage.getItem("token")
     const { data, isLoading, error } = useUser(token)
 
-    if (error) {
+    if (error || !data || data?.disabled === true) {
         router.push("/login")
     }
     if (isLoading) {
         <LoadingPage/>
     }
+
     return (
         <div>
-        
+            <h1>Welcome {data.username}</h1>
         </div>
     )
 }
